63aff8447f18c4a98de2fb997129b2b12d1426bb
root <root@markalbrechtgpu.c.speech-1518535569363.internal>
2018-04-30 20:50:36 +0000

    fixed lesson 7 setup

--- getdata.sh
+++ getdata.sh
@@ -5,19 +5,22 @@ apt install unzip -y
 ### get dogs cats data if it doesn't exist
 if [ ! -e ../data/dogscats ]
 	then
-		cd ../data && curl http://files.fast.ai/data/dogscats.zip --output dogscats.zip	
-		cd ../data && unzip -d . dogscats.zip 
-		cd ../data && rm dogscats.zip
+		cd ../data
+		curl http://files.fast.ai/data/dogscats.zip --output dogscats.zip	
+		unzip -d . dogscats.zip 
+		rm dogscats.zip
 fi
 
 
 ### get rossman data
 if [ ! -e ../data/rossmann ]
         then
-                cd ../data && curl http://files.fast.ai/part2/lesson14/rossmann.tgz  --output rossmann.tgz 
-                cd ../data && mkdir rossmann 
-		cd ../data && mv rossmann.tgz rossmann/
-                cd ../data/rossmann && tar -xzf rossmann.tgz
+                cd ../data
+		curl http://files.fast.ai/part2/lesson14/rossmann.tgz  --output rossmann.tgz 
+                mkdir rossmann 
+		mv rossmann.tgz rossmann/
+                cd ../data/rossmann
+		tar -xzf rossmann.tgz
 		cd ..
 fi
 
@@ -25,9 +28,10 @@ fi
 # get movielens data
 if [ ! -e ../data/ml-latest-small ]
         then
-		cd ../data && curl http://files.grouplens.org/datasets/movielens/ml-latest-small.zip --output ml-latest-small.zip
-		cd ../data && unzip -d . ml-latest-small.zip 
-		cd ../data && rm ml-latest-small.zip
+		cd ../data
+		curl http://files.grouplens.org/datasets/movielens/ml-latest-small.zip --output ml-latest-small.zip
+		unzip -d . ml-latest-small.zip 
+		rm ml-latest-small.zip
 fi
 
 # get imdb data
@@ -38,3 +42,12 @@ if [ ! -e ../data/aclImdb ]
                 cd ..
 fi
 
+### get cifar data
+if [ ! -e ../data/cifar ]
+        then
+		cd ../data
+                curl -L http://pjreddie.com/media/files/cifar.tgz --output cifar.tgz 
+		tar -xzf cifar.tgz
+		rm cifar.tgz
+                cd ..
+fi
