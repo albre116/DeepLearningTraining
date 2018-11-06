a65fb910690ebe2472c14ce8872196e5394d105e
root <root@carlson-demo-gpu.c.speech-1518535569363.internal>
2018-11-06 16:45:27 +0000

    updated build

--- setup/getdrivers_dockerce.sh
+++ setup/getdrivers_dockerce.sh
@@ -30,3 +30,10 @@ sudo apt-get install -y docker-ce
 # Install nvidia-docker2 and reload the Docker daemon configuration
 sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
+
+# Install docker compose
+curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Test install
+docker-compose --version
